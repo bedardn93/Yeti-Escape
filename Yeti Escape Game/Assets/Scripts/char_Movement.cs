32e61f6d7055606fb0241c8daa2c20e834accd66
tinytunafish <chridgely@gmail.com>
2015-05-25 12:32:14 -0400

    Added Initial Script and Materials
    Needed to establish folder hierarchy

---
+++ Yeti Escape Game/Assets/Scripts/char_Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class char_Movement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed = 15f;
+
+	//Used for intialization
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//Updates every frame after physics simulation 
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);	 
+	}	
+}
+
