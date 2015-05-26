600c9f12708c3fddf326bb2c6c09336e099148c1
tinytunafish <chridgely@gmail.com>
2015-05-25 20:36:10 -0400

    Finished Character Controller (Movement)

---
+++ Yeti Escape Game/Assets/Scripts/cameraAction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraAction : MonoBehaviour {
+
+	public Transform target;
+	public float verticalSpeed = 10.0f;
+	public float maxY = 50f;
+	public float minY = -50.0f;
+
+	private Vector3 v3Rotate;
+	
+	// Use this for initialization
+	void Start () {
+		transform.localEulerAngles = v3Rotate;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Vertical Look Rotation
+		vLook ();
+	}
+
+	/*
+	 * Name: Vertical Look
+	 * Purpose: Used to transform the camera vertical rotation/ X rotation
+	 * Arguments: None
+	 * Author: Ridgely
+	 */
+	void vLook()
+	{
+		//Defaults to inverted mouse, this makes it normal
+		v3Rotate.x += Input.GetAxis ("Mouse Y") * verticalSpeed * -1f;
+
+		//This sets the limits of vertical look
+		v3Rotate.x = Mathf.Clamp (v3Rotate.x, minY, maxY);
+
+		//Transforms the camera up and down using Euler angles
+		transform.localEulerAngles = v3Rotate;
+	}
+
+}
--- Yeti Escape Game/Assets/Scripts/char_Movement.cs
+++ Yeti Escape Game/Assets/Scripts/char_Movement.cs
@@ -2,23 +2,100 @@
 using System.Collections;
 
 public class char_Movement : MonoBehaviour {
-
-	private Rigidbody rb;
-	public float speed = 15f;
-
-	//Used for intialization
-	void Start(){
-		rb = GetComponent<Rigidbody> ();
-	}
+	
+	public float moveSpeed = 15f;
+	public float rotateSpeed = 2f;
 
 	//Updates every frame after physics simulation 
-	void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		
-		rb.AddForce (movement * speed);	 
+	void FixedUpdate()
+	{
+		movement ();
+		turn ();
 	}	
+
+	/*
+	 * Name: Old Move
+	 * Purpose: Old movement method, no camera direction
+	 * Arguments: None
+	 * Author: Ridgely
+	 */
+	void movement()
+	{
+		// Take key input
+		float moveHorizontal = Input.GetAxis ("Horizontal") * moveSpeed;
+		float moveVertical = Input.GetAxis ("Vertical") * moveSpeed;
+
+		// Move Right
+		if (moveHorizontal > 0) {
+			moveRight (moveHorizontal);
+		}
+
+		// Move Left
+		if (moveHorizontal < 0) {
+			moveLeft (moveHorizontal);
+		}
+
+		// Move Forward
+		if (moveVertical > 0) {
+			moveForward (moveVertical);
+		}
+
+		// Move Backward
+		if (moveVertical < 0) {
+			moveBack (moveVertical);
+		} 
+	}
+
+	/*
+	 * Name: Move Forward
+	 * Purpose: Moves the object forward based on local orientation
+	 * Arguments: Speed of movement in float
+	 * Author: Ridgely
+	 */
+	void moveForward(float speed) {
+		transform.localPosition += transform.forward * Mathf.Abs (speed) * Time.deltaTime;
+	}
+
+	/*
+	 * Name: Move Backward
+	 * Purpose: Moves the object backward based on local orientation
+	 * Arguments: Speed of movement in float
+	 * Author: Ridgely
+	 */
+	void moveBack(float speed) {
+		transform.localPosition -= transform.forward * Mathf.Abs (speed) * Time.deltaTime;
+	}
+
+	/*
+	 * Name: Move Right
+	 * Purpose: Moves the object right based on local orientation
+	 * Arguments: Speed of movement in float
+	 * Author: Ridgely
+	 */
+	void moveRight(float speed) {
+		transform.localPosition += transform.right * Mathf.Abs (speed) * Time.deltaTime;
+	}
+
+	/*
+	 * Name: Move Left
+	 * Purpose: Moves the object left based on local orientation
+	 * Arguments: Speed of movement in float
+	 * Author: Ridgely
+	 */
+	void moveLeft(float speed) {
+		transform.localPosition -= transform.right * Mathf.Abs (speed) * Time.deltaTime;
+	}
+
+	/*
+	 * Name: Turn
+	 * Purpose: Rotates the object
+	 * Arguments: None
+	 * Author: Ridgely
+	 */
+	void turn()
+	{
+		float rotHorizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+		transform.Rotate (0, rotHorizontal, 0);
+	}
 }
 
